f4824b6a85e6aaceafd728d28678752a8676211d
Capzera <1786126188@qq.com>
2023-01-29 21:55:50 +0800

    21:55

--- widget.cpp
+++ widget.cpp
@@ -1,10 +1,9 @@
 #include "widget.h"
 
 Widget::Widget(QWidget *parent): QWidget(parent) {
-jkdxfhfv kjaf hjkdsafjaskdlfh jkdalsf hjaksldf
-        666666666666666666666666
-        666666666666666666666666
-        66666
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
 }
 
 Widget::~Widget() {}
