dcadf701e5bac23b7fb328bf3fa4448aaf04707b
cnl12345 <1258940442@qq.com>
2023-01-29 21:41:11 +0800

    6666

--- widget.cpp
+++ widget.cpp
@@ -2,6 +2,9 @@
 
 Widget::Widget(QWidget *parent): QWidget(parent) {
 jkdxfhfv kjaf hjkdsafjaskdlfh jkdalsf hjaksldf
+        666666666666666666666666
+        666666666666666666666666
+        66666
 }
 
 Widget::~Widget() {}
