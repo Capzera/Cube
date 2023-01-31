6d10af5377bdd76346b1bea7990fef671fef3d65
cnl12345 <1258940442@qq.com>
2023-01-31 19:59:08 +0800

    19:59

--- barrier.h
+++ barrier.h
@@ -3,17 +3,28 @@
 
 #include <QPoint>
 
+enum BARRIER_STATE{
+    SOLID
+};
+
+enum BARRIER_COLOR{
+    BLACK
+};
+
 class BARRIER
 {
 public:
     BARRIER();
-    BARRIER(int x, int y);
+    BARRIER(int x, int y, int color, int state);
     ~BARRIER();
 public:
     bool isBarrier();
     QPoint getPos();
+
 public:
     QPoint _pos;
+    enum BARRIER_COLOR _color;
+    enum BARRIER_STATE _state;
 };
 
 #endif // BARRIER_H
